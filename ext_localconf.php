be36474bb61a3c6136dc1aea1d9b5c7bcbec17ae
Felix Semmler <felix.semmler@aoe.com>
2021-06-02 08:54:18 +0200

    [REFACTORING] Use short array syntax

--- ext_localconf.php
+++ ext_localconf.php
@@ -5,10 +5,10 @@ if (!defined('TYPO3_MODE')) {
 
 if (TYPO3_MODE == 'BE') {
 	// register scheduler-task to update refindex of TYPO3
-	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks'][\Aoe\UpdateRefindex\Scheduler\UpdateRefIndexTask::class] = array (
+	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks'][\Aoe\UpdateRefindex\Scheduler\UpdateRefIndexTask::class] = [
 		'extension'        => 'update_refindex',
 		'title'            => 'LLL:EXT:update_refindex/Resources/Private/Language/locallang_db.xml:scheduler_task_updateRefindex.name',
 		'description'      => 'LLL:EXT:update_refindex/Resources/Private/Language/locallang_db.xml:scheduler_task_updateRefindex.description',
 		'additionalFields' => \Aoe\UpdateRefindex\Scheduler\UpdateRefIndexAdditionalFields::class,
-	);
+    ];
 }
