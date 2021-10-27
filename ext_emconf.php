3b24a89657bf7807ed2ef1d61309e3716468548f
Thomas Layh <thomas.layh@aoe.com>
2021-10-27 09:42:07 +0200

    [RELEASE] update_refindex version 9.0.1

--- ext_emconf.php
+++ ext_emconf.php
@@ -25,10 +25,10 @@ $EM_CONF[$_EXTKEY] = [
     'modify_tables' => '',
     'clearCacheOnLoad' => 1,
     'lockType' => '',
-    'version' => '9.0.0',
+    'version' => '9.0.1',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.0-10.4.99',
+            'typo3' => '9.5.0-11.5.99',
         ],
         'conflicts' => [],
         'suggests' => [],
