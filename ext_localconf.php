d2d929e6287ea2265106a304e0a95949beb34c1e
Felix Semmler <felix.semmler@aoe.com>
2022-06-17 14:40:25 +0200

    [REFACTORING] Minor changes and code-quality improvements (#12)

--- ext_localconf.php
+++ ext_localconf.php
@@ -7,8 +7,8 @@ if (TYPO3_MODE == 'BE') {
 	// register scheduler-task to update refindex of TYPO3
 	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks'][\Aoe\UpdateRefindex\Scheduler\UpdateRefIndexTask::class] = [
 		'extension'        => 'update_refindex',
-		'title'            => 'LLL:EXT:update_refindex/Resources/Private/Language/locallang_db.xml:scheduler_task_updateRefindex.name',
-		'description'      => 'LLL:EXT:update_refindex/Resources/Private/Language/locallang_db.xml:scheduler_task_updateRefindex.description',
+		'title'            => 'LLL:EXT:update_refindex/Resources/Private/Language/locallang.xlf:scheduler_task_updateRefindex.name',
+		'description'      => 'LLL:EXT:update_refindex/Resources/Private/Language/locallang.xlf:scheduler_task_updateRefindex.description',
 		'additionalFields' => \Aoe\UpdateRefindex\Scheduler\UpdateRefIndexAdditionalFields::class,
     ];
 }
