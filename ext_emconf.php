4a20ec9d8efce6abe3fc936f53a28cc3eeccb1b5
Thomas Layh <thomas.layh@aoe.com>
2024-04-22 13:54:37 +0200

    Update ext_emconf.php with new versions

--- ext_emconf.php
+++ ext_emconf.php
@@ -25,10 +25,10 @@ $EM_CONF[$_EXTKEY] = [
     'modify_tables' => '',
     'clearCacheOnLoad' => 1,
     'lockType' => '',
-    'version' => '9.1.2',
+    'version' => '11.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.0-11.5.99',
+            'typo3' => '11.5.0-11.5.99',
         ],
         'conflicts' => [],
         'suggests' => [],
